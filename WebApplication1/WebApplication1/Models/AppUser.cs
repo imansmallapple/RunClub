aae2221c5932b134f5a02e11db3244b6d1faaed7
Jiayang Yan <01166416@pw.edu.pl>
2024-11-16 22:50:41 +0100

    move current project

---
+++ WebApplication1/WebApplication1/Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Key]
+        public string Id { get; set; }
+        public int? Pace { get; set; }
+        //int?它可以是整数，也可以为空null
+        public int? Mileage { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+
+    }
+}
